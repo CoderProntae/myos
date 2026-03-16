877e7902f42019b09ae3aa892cfac438ee0f9db8
pr-codermcun <oa2300743@gmail.com>
2026-03-16 23:47:21 +0300

    Update setup.c

--- src/setup.c
+++ src/setup.c
@@ -163,6 +163,7 @@ int setup_run(void) {
 
         /* Tiklama */
         if (ms.click && hover1) {
+            mouse_clear_click();
             disk_check_screen();
             install_screen();
             return 1;
