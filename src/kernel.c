30538b7e3e7d02c8abebbf64e55582fc1ccc76d0
pr-codermcun <oa2300743@gmail.com>
2026-03-17 00:28:14 +0300

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -2,11 +2,13 @@
 #include "mouse.h"
 #include "setup.h"
 #include "gui.h"
+#include "pci.h"
 
 void kernel_main(uint32_t magic, multiboot_info_t* mbi) {
     (void)magic;
     vesa_init(mbi);
     mouse_init();
+    pci_init();
 
     setup_run();
     gui_run();
