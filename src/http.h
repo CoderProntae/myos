9218b6e93553527e31a99d9a93579f6cd05b9262
pr-codermcun <oa2300743@gmail.com>
2026-03-17 02:49:10 +0300

    Update http.h

--- src/http.h
+++ src/http.h
@@ -3,7 +3,8 @@
 
 #include <stdint.h>
 
-#define HTTP_MAX_BODY 3072
+#define HTTP_MAX_BODY 8192
+#define HTTP_MAX_HEADER 512
 
 typedef struct {
     int      status_code;
@@ -12,6 +13,7 @@ typedef struct {
     char     body[HTTP_MAX_BODY];
     int      body_len;
     int      error;
+    char     error_msg[64];
 } http_response_t;
 
 int  http_get(const char* host, const char* path, http_response_t* resp);
