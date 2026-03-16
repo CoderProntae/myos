0e67a915f2627f60af0d6c7ad7a8592b414ca153
pr-codermcun <oa2300743@gmail.com>
2026-03-16 20:45:53 +0300

    Update vga.c

--- src/vga.c
+++ src/vga.c
@@ -33,14 +33,6 @@ void vga_clear(void) {
     vga_update_cursor();
 }
 
-void vga_clear_color(uint8_t fg, uint8_t bg) {
-    uint8_t c = make_color(fg, bg);
-    for (int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++)
-        vga_buffer[i] = make_entry(' ', c);
-    cursor_x = 0;
-    cursor_y = 0;
-}
-
 void vga_set_color(uint8_t fg, uint8_t bg) {
     current_color = make_color(fg, bg);
 }
@@ -69,20 +61,8 @@ void vga_putchar(char c) {
     vga_update_cursor();
 }
 
-void vga_putchar_at(char c, uint8_t color, int x, int y) {
-    if (x >= 0 && x < VGA_WIDTH && y >= 0 && y < VGA_HEIGHT)
-        vga_buffer[y * VGA_WIDTH + x] = make_entry(c, color);
-}
-
 void vga_print(const char* str) {
     while (*str) vga_putchar(*str++);
-}
-
-void vga_print_at(const char* str, uint8_t fg, uint8_t bg, int x, int y) {
-    uint8_t color = make_color(fg, bg);
-    while (*str && x < VGA_WIDTH) {
-        vga_putchar_at(*str++, color, x++, y);
-    }
 }
 
 void vga_print_colored(const char* str, uint8_t fg, uint8_t bg) {
@@ -101,52 +81,13 @@ void vga_backspace(void) {
 
 void vga_update_cursor(void) {
     uint16_t pos = cursor_y * VGA_WIDTH + cursor_x;
-    outb(0x3D4, 14); outb(0x3D5, (pos >> 8) & 0xFF);
-    outb(0x3D4, 15); outb(0x3D5, pos & 0xFF);
+    outb(0x3D4, 14);
+    outb(0x3D5, (uint8_t)(pos >> 8));
+    outb(0x3D4, 15);
+    outb(0x3D5, (uint8_t)(pos & 0xFF));
 }
 
 void vga_hide_cursor(void) {
     outb(0x3D4, 0x0A);
     outb(0x3D5, 0x20);
-}
-
-void vga_fill_row(int row, uint8_t fg, uint8_t bg) {
-    uint8_t color = make_color(fg, bg);
-    for (int x = 0; x < VGA_WIDTH; x++)
-        vga_buffer[row * VGA_WIDTH + x] = make_entry(' ', color);
-}
-
-void vga_fill_rect(int x1, int y1, int x2, int y2, uint8_t fg, uint8_t bg) {
-    uint8_t color = make_color(fg, bg);
-    for (int y = y1; y <= y2 && y < VGA_HEIGHT; y++)
-        for (int x = x1; x <= x2 && x < VGA_WIDTH; x++)
-            vga_buffer[y * VGA_WIDTH + x] = make_entry(' ', color);
-}
-
-void vga_draw_box(int x1, int y1, int x2, int y2, uint8_t fg, uint8_t bg) {
-    uint8_t color = make_color(fg, bg);
-    vga_putchar_at('+', color, x1, y1);
-    vga_putchar_at('+', color, x2, y1);
-    vga_putchar_at('+', color, x1, y2);
-    vga_putchar_at('+', color, x2, y2);
-    for (int x = x1 + 1; x < x2; x++) {
-        vga_putchar_at('-', color, x, y1);
-        vga_putchar_at('-', color, x, y2);
-    }
-    for (int y = y1 + 1; y < y2; y++) {
-        vga_putchar_at('|', color, x1, y);
-        vga_putchar_at('|', color, x2, y);
-    }
-    for (int y = y1 + 1; y < y2; y++)
-        for (int x = x1 + 1; x < x2; x++)
-            vga_putchar_at(' ', color, x, y);
-}
-
-uint16_t vga_get_entry(int x, int y) {
-    return vga_buffer[y * VGA_WIDTH + x];
-}
-
-void vga_set_entry(int x, int y, uint16_t entry) {
-    if (x >= 0 && x < VGA_WIDTH && y >= 0 && y < VGA_HEIGHT)
-        vga_buffer[y * VGA_WIDTH + x] = entry;
 }
