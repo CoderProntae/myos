49298cdcacd0a6e08cc4d22dbcbaab4ce49b8745
pr-codermcun <oa2300743@gmail.com>
2026-03-16 20:09:56 +0300

    Update vga.h

--- src/vga.h
+++ src/vga.h
@@ -13,18 +13,20 @@ enum vga_color {
 
 void vga_init(void);
 void vga_clear(void);
+void vga_clear_color(uint8_t fg, uint8_t bg);
 void vga_set_color(uint8_t fg, uint8_t bg);
 void vga_putchar(char c);
+void vga_putchar_at(char c, uint8_t color, int x, int y);
 void vga_print(const char* str);
+void vga_print_at(const char* str, uint8_t fg, uint8_t bg, int x, int y);
 void vga_print_colored(const char* str, uint8_t fg, uint8_t bg);
 void vga_backspace(void);
 void vga_update_cursor(void);
-void vga_show_cursor(void);
 void vga_hide_cursor(void);
-
-void vga_putentry_at(char c, uint8_t fg, uint8_t bg, int x, int y);
-void vga_write_at(const char* str, int x, int y, uint8_t fg, uint8_t bg);
-uint16_t vga_getentry_at(int x, int y);
-void vga_setentry_at(uint16_t entry, int x, int y);
+void vga_fill_row(int row, uint8_t fg, uint8_t bg);
+void vga_fill_rect(int x1, int y1, int x2, int y2, uint8_t fg, uint8_t bg);
+void vga_draw_box(int x1, int y1, int x2, int y2, uint8_t fg, uint8_t bg);
+uint16_t vga_get_entry(int x, int y);
+void vga_set_entry(int x, int y, uint16_t entry);
 
 #endif
