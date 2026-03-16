4182bbbdf21dba186fe8315316e08035e319f673
pr-codermcun <oa2300743@gmail.com>
2026-03-16 20:36:01 +0300

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -1,19 +1,25 @@
-MBALIGN  equ 1 << 0
-MEMINFO  equ 1 << 1
-FLAGS    equ MBALIGN | MEMINFO
-MAGIC    equ 0x1BADB002
-CHECKSUM equ -(MAGIC + FLAGS)
+MBALIGN    equ 1 << 0
+MEMINFO    equ 1 << 1
+VIDMODE    equ 1 << 2
+FLAGS      equ MBALIGN | MEMINFO | VIDMODE
+MAGIC      equ 0x1BADB002
+CHECKSUM   equ -(MAGIC + FLAGS)
 
 section .multiboot
 align 4
     dd MAGIC
     dd FLAGS
     dd CHECKSUM
+    dd 0, 0, 0, 0, 0       ; aout kludge (unused)
+    dd 0                    ; mode_type: 0 = linear graphics
+    dd 800                  ; width
+    dd 600                  ; height
+    dd 32                   ; depth (32-bit color)
 
 section .bss
 align 16
 stack_bottom:
-    resb 16384
+    resb 32768
 stack_top:
 
 section .text
@@ -22,6 +28,8 @@ extern kernel_main
 
 _start:
     mov esp, stack_top
+    push ebx                ; multiboot info pointer
+    push eax                ; magic number
     call kernel_main
     cli
 .hang:
