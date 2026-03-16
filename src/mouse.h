9deb40d73fc4bf39713987a2014a3befae9f6020
pr-codermcun <oa2300743@gmail.com>
2026-03-16 20:11:11 +0300

    Update mouse.h

--- src/mouse.h
+++ src/mouse.h
@@ -6,13 +6,14 @@
 typedef struct {
     int x;
     int y;
+    int col;
+    int row;
     uint8_t left;
     uint8_t right;
     uint8_t middle;
 } mouse_state_t;
 
 void mouse_init(void);
-int mouse_poll(mouse_state_t* state);
-void mouse_get_state(mouse_state_t* state);
+void mouse_poll(mouse_state_t* state);
 
 #endif
