4fbbcce2ed26faea408d2921a6fe6483980fe5be
pr-codermcun <oa2300743@gmail.com>
2026-03-17 22:59:15 +0300

    Delete userspace/include directory

--- userspace/include/assert.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ASSERT_H
-#define _ASSERT_H
-
-void __assert_fail(const char* expr, const char* file, int line);
-
-#define assert(expr) \
-    ((expr) ? (void)0 : __assert_fail(#expr, __FILE__, __LINE__))
-
-#endif
