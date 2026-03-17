4a6e3627da162d168c9d03bb0a6916edee90b960
pr-codermcun <oa2300743@gmail.com>
2026-03-17 03:25:01 +0300

    Update heap.h

--- src/heap.h
+++ src/heap.h
@@ -3,13 +3,11 @@
 
 #include <stdint.h>
 
-/* Heap boyutu: 16 MB */
 #define HEAP_SIZE (16 * 1024 * 1024)
 
-/* Blok header */
 typedef struct block_header {
-    uint32_t size;            /* Veri alani boyutu (header haric) */
-    uint32_t magic;           /* 0xDEADBEEF = kullaniliyor, 0xFEEDFACE = bos */
+    uint32_t size;
+    uint32_t magic;
     struct block_header* next;
     struct block_header* prev;
 } block_header_t;
@@ -19,17 +17,22 @@ typedef struct block_header {
 #define BLOCK_HDR_SIZE  sizeof(block_header_t)
 #define MIN_BLOCK_SIZE  16
 
-/* Fonksiyonlar */
 void  heap_init(void);
 void* kmalloc(uint32_t size);
 void  kfree(void* ptr);
 void* krealloc(void* ptr, uint32_t new_size);
 void* kcalloc(uint32_t count, uint32_t size);
 
-/* Bilgi */
 uint32_t heap_get_total(void);
 uint32_t heap_get_used(void);
 uint32_t heap_get_free(void);
 uint32_t heap_get_block_count(void);
+
+/* Gercek RAM bilgisi */
+void     ram_detect(uint32_t mem_lower, uint32_t mem_upper);
+uint32_t ram_get_total_kb(void);
+uint32_t ram_get_total_mb(void);
+uint32_t ram_get_lower_kb(void);
+uint32_t ram_get_upper_kb(void);
 
 #endif
