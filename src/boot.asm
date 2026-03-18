5473eb6bd8396ca75308f6b558c928ed8a8ed6fc
pr-codermcun <oa2300743@gmail.com>
2026-03-18 23:17:37 +0300

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -1,36 +1,64 @@
-MBALIGN    equ 1 << 0
-MEMINFO    equ 1 << 1
-VIDMODE    equ 1 << 2
-FLAGS      equ MBALIGN | MEMINFO | VIDMODE
-MAGIC      equ 0x1BADB002
-CHECKSUM   equ -(MAGIC + FLAGS)
+; ============================================
+;         MyOS Boot - Multiboot
+; ============================================
 
+; Multiboot sabitleri
+MBALIGN     equ 1 << 0                 ; Sayfa hizalama
+MEMINFO     equ 1 << 1                 ; Bellek bilgisi
+VIDMODE     equ 1 << 2                 ; Video modu bilgisi
+FLAGS       equ MBALIGN | MEMINFO | VIDMODE
+MAGIC       equ 0x1BADB002             ; Multiboot magic
+CHECKSUM    equ -(MAGIC + FLAGS)
+
+; Multiboot header
 section .multiboot
 align 4
     dd MAGIC
     dd FLAGS
     dd CHECKSUM
-    dd 0, 0, 0, 0, 0       ; aout kludge (unused)
-    dd 0                    ; mode_type: 0 = linear graphics
-    dd 800                  ; width
-    dd 600                  ; height
-    dd 32                   ; depth (32-bit color)
+    
+    ; aout kludge (boş)
+    dd 0    ; header_addr
+    dd 0    ; load_addr
+    dd 0    ; load_end_addr
+    dd 0    ; bss_end_addr
+    dd 0    ; entry_addr
+    
+    ; Video mode
+    dd 0    ; mode_type (0 = linear graphics)
+    dd 800  ; width
+    dd 600  ; height
+    dd 32   ; depth (bpp)
 
+; Stack
 section .bss
 align 16
 stack_bottom:
-    resb 32768
+    resb 16384          ; 16 KB stack
 stack_top:
 
+; Boot kodu
 section .text
 global _start
 extern kernel_main
 
 _start:
+    ; Stack pointer'ı ayarla
     mov esp, stack_top
-    push ebx                ; multiboot info pointer
-    push eax                ; magic number
+    
+    ; EFLAGS temizle
+    push 0
+    popf
+    
+    ; kernel_main(multiboot_info_t* mboot, uint32_t magic)
+    ; C calling convention: parametreler stack'e sağdan sola
+    push eax            ; magic number (EAX = 0x2BADB002)
+    push ebx            ; multiboot info pointer (EBX)
+    
+    ; kernel_main çağır
     call kernel_main
+    
+    ; kernel_main dönerse (dönmemeli)
     cli
 .hang:
     hlt
