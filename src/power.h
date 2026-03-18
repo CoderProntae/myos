fe0bcb780bf1727408b72c2a2efa41523ede24a6
pr-codermcun <oa2300743@gmail.com>
2026-03-18 20:48:09 +0300

    Create power.h

---
+++ src/power.h
@@ -0,0 +1,26 @@
+#ifndef POWER_H
+#define POWER_H
+
+#include <stdint.h>
+
+// Port I/O fonksiyonları
+static inline void outb(uint16_t port, uint8_t val) {
+    asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline void outw(uint16_t port, uint16_t val) {
+    asm volatile ("outw %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
+
+// Power fonksiyonları
+void shutdown(void);
+void reboot(void);
+void halt(void);
+
+#endif
